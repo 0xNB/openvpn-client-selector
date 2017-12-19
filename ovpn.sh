df0fb026d5d7a9db9a416a9aaf21673916e2912c
Einstein <ungep@student.kit.edu>
2017-12-19 23:37:17 +0100

    Added some regex

--- ovpn.sh
+++ ovpn.sh
@@ -15,13 +15,18 @@ do
 done
 
 arr=($configs)
+re='^[0-9]+$'
 
 read -p "Select an openvpn server to connect to: " selection
 echo
-if [ "$selection" -gt "$((${#arr[@]} - 1))" ] || [ "$selection" -lt 0 ]
+if [ "$selection" = "" ] || ! [[ $selection =~ $re ]]
+then
+        echo Not a number, exiting.
+        exit 1
+elif [ "$selection" -gt "$((${#arr[@]} - 1))" ] || [ "$selection" -lt 0 ]
 then
 	echo Selection out of range, exiting
-	exit
+	exit 1
 fi
 echo ${arr[$selection]%.conf} selected, establishing connection with config file ${arr[$selection]}
 sudo openvpn --config /etc/openvpn/client/${arr[$selection]}
